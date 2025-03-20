ee239c133f881155435c5a480802d9711140ce54
delalegion <nielotweb@gmail.com>
2025-03-20 23:15:37 +0100

    Fix config images

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,30 +1,4 @@
 module.exports = [
-    {
-        name: 'strapi::security',
-        config: {
-          contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-              'connect-src': ["'self'", 'https:'],
-              'img-src': [
-                "'self'",
-                'data:',
-                'blob:',
-                'market-assets.strapi.io',
-                'yourBucketName.s3.yourRegion.amazonaws.com',
-              ],
-              'media-src': [
-                "'self'",
-                'data:',
-                'blob:',
-                'market-assets.strapi.io',
-                'yourBucketName.s3.yourRegion.amazonaws.com',
-              ],
-              upgradeInsecureRequests: null,
-            },
-          },
-        },
-    },
     'strapi::logger',
     'strapi::errors',
     'strapi::security',
