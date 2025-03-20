d165bdf2f30081a3570964bb1fc002cb114a1c5b
delalegion <nielotweb@gmail.com>
2025-03-20 21:55:15 +0100

    Add upload plugin

---
+++ config/env/production/middlewares.js
@@ -0,0 +1,39 @@
+module.exports = [
+    {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            useDefaults: true,
+            directives: {
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'market-assets.strapi.io',
+                'yourBucketName.s3.yourRegion.amazonaws.com',
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'market-assets.strapi.io',
+                'yourBucketName.s3.yourRegion.amazonaws.com',
+              ],
+              upgradeInsecureRequests: null,
+            },
+          },
+        },
+    },
+    'strapi::logger',
+    'strapi::errors',
+    'strapi::security',
+    'strapi::cors',
+    'strapi::poweredBy',
+    'strapi::query',
+    'strapi::body',
+    'strapi::session',
+    'strapi::favicon',
+    'strapi::public',
+  ];
+  
