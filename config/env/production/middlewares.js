f33e703c29dd4d59323e7db275f6e0b3586d708d
delalegion <nielotweb@gmail.com>
2025-03-20 23:23:57 +0100

    Fix config middleware

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,6 +1,17 @@
-module.exports = [
+module.exports = ({ env }) => [
     'strapi::logger',
     'strapi::errors',
+    {
+        name: 'strapi::security',
+        config: {
+          contentSecurityPolicy: {
+            directives: {
+              'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
+              'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+            },
+          }
+        },
+    },
     'strapi::security',
     'strapi::cors',
     'strapi::poweredBy',
