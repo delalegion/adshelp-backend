0dc4a56d088b58a140a1307ebfd4833725d574c9
delalegion <nielotweb@gmail.com>
2025-03-20 23:45:11 +0100

    Fix config middlewares

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -5,13 +5,30 @@ module.exports = ({ env }) => [
         name: 'strapi::security',
         config: {
           contentSecurityPolicy: {
+            useDefaults: true,
             directives: {
-              'script-src': ["'self'", "'unsafe-inline'", 'cdn.jsdelivr.net'],
-              'img-src': ["'self'", 'data:', 'cdn.jsdelivr.net', 'strapi.io', `${env('DO_SPACE_BUCKET')}.s3.${env('DO_SPACE_REGION')}.amazonaws.com`, `${env('DO_SPACE_BUCKET')}.${env('DO_SPACE_REGION')}.cdn.digitaloceanspaces.com`],
+              'connect-src': ["'self'", 'https:'],
+              'img-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'market-assets.strapi.io',
+                `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_REGION")}.cdn.digitaloceanspaces.com`,
+                `${env("DO_SPACE_BUCKET")}.s3.${env("DO_SPACE_REGION")}.amazonaws.com`,
+              ],
+              'media-src': [
+                "'self'",
+                'data:',
+                'blob:',
+                'market-assets.strapi.io',
+                `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_REGION")}.cdn.digitaloceanspaces.com`,
+                `${env("DO_SPACE_BUCKET")}.s3.${env("DO_SPACE_REGION")}.amazonaws.com`,
+              ],
+              upgradeInsecureRequests: null,
             },
-          }
+          },
         },
-    },
+      },
     'strapi::security',
     'strapi::cors',
     'strapi::poweredBy',
