7b6cf62ca7368809e7d909d81b45950bb45aec01
delalegion <nielotweb@gmail.com>
2025-04-05 22:50:44 +0200

    Change content type of articles

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -424,7 +424,7 @@ export interface ApiArticleArticle extends Struct.CollectionTypeSchema {
       Schema.Attribute.Private;
     description: Schema.Attribute.Text &
       Schema.Attribute.SetMinMaxLength<{
-        maxLength: 80;
+        maxLength: 260;
       }>;
     locale: Schema.Attribute.String & Schema.Attribute.Private;
     localizations: Schema.Attribute.Relation<
